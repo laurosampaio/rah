771b2171bb08cadda3d26cbc0f232c0d560b7019
lauro <lauro.alexandre@gmail.com>
2024-05-18 19:27:26 -0300

    - Modelo carregando com sucesso

--- lib/pages/recognitionpage.dart
+++ lib/pages/recognitionpage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'dart:async';
-import 'dart:typed_data';
 
 class RecognitionPage extends StatefulWidget {
   static const routeName = '/recognition';
@@ -14,118 +13,132 @@ class RecognitionPage extends StatefulWidget {
 }
 
 class _RecognitionPageState extends State<RecognitionPage> {
-  late Interpreter _interpreter;
-  List<double>? _accelerometerValues;
-  String _prediction = 'Indefinido';
-  String _erro = '';
-  bool _isRecognizing = false;
-  late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
+  List<String> _actions = [];
+  bool _isModelLoaded = false;
+  bool _isListening = false;
+  bool _isStartButtonDisabled = false;
+  Interpreter? _interpreter;
+  List<double> _accelerometerValues = [0.0, 0.0, 0.0];
 
   @override
   void initState() {
     super.initState();
-    _loadModel();
+    loadModel().then((value) {
+      setState(() {
+        _isModelLoaded = true;
+      });
+    });
+    listenToAccelerometer();
   }
 
-  Future<void> _loadModel() async {
+  // Load TensorFlow Lite model
+  Future<void> loadModel() async {
     try {
       _interpreter = await Interpreter.fromAsset('assets/rah_model.tflite');
+      print('Model loaded successfully');
     } catch (e) {
-      _erro = 'Erro ao carregar modelo model: $e';
-      print(_erro);
+      print('Failed to load model: $e');
     }
   }
 
-  void _startRecognition() {
-    setState(() {
-      _isRecognizing = true;
-    });
-    _accelerometerSubscription =
-        accelerometerEvents.listen((AccelerometerEvent event) {
-      setState(() {
-        _accelerometerValues = <double>[event.x, event.y, event.z];
-        _runModel();
-      });
-    });
-  }
-
-  void _stopRecognition() {
-    setState(() {
-      _isRecognizing = false;
-      _accelerometerValues = null;
-      _prediction = 'Indefinido';
-      _erro = '';
-    });
-    _accelerometerSubscription.cancel();
-  }
-
-  void _runModel() {
-    if (_accelerometerValues == null || !_isRecognizing) return;
-
-    // Prepare input data as Float32List
-    var input = _accelerometerValues!.map((e) => e.toDouble()).toList();
-    var inputBuffer = Float32List.fromList(input).buffer.asUint8List();
-
-    // Create an output buffer
-    var outputBuffer = Float32List(10).buffer.asUint8List();
+  // Predict action
+  Future<void> predictAction(List<List<double>> input) async {
+    if (!_isModelLoaded || _interpreter == null) return;
 
     try {
-      // Run inference
-      _interpreter.run(inputBuffer, outputBuffer);
+      List<Object?> inputList = [input];
+      List<Object?> outputList = [List.filled(1, 0.0, growable: true)];
 
-      // Process output
-      var output = outputBuffer.buffer.asFloat32List();
-      var predictedIndex = output
-          .indexOf(output.reduce((curr, next) => curr > next ? curr : next));
+      _interpreter!.run(inputList, outputList);
+
       setState(() {
-        _prediction = 'Ação => $predictedIndex';
+        _actions = outputList[0] as List<String>;
       });
     } catch (e) {
-      _erro = 'Erro executando modelo: $e';
-      print(_erro);
+      print('Failed to run inference: $e');
     }
+  }
+
+  // Listen to accelerometer
+  void listenToAccelerometer() {
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        _accelerometerValues = [event.x ?? 0.0, event.y ?? 0.0, event.z ?? 0.0];
+      });
+      if (_isListening) {
+        List<double> accelerometerData = _accelerometerValues;
+        predictAction([accelerometerData]);
+      }
+    });
+  }
+
+  void startListeningToAccelerometer() {
+    _isListening = true;
+    setState(() {
+      _isStartButtonDisabled = true;
+    });
+  }
+
+  void stopListeningToAccelerometer() {
+    _isListening = false;
+    setState(() {
+      _isStartButtonDisabled = false;
+    });
   }
 
   @override
   void dispose() {
-    _interpreter.close();
-    if (_isRecognizing) {
-      _accelerometerSubscription.cancel();
+    if (_interpreter != null) {
+      _interpreter!.close();
     }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final accelerometer =
-        _accelerometerValues?.map((v) => v.toStringAsFixed(1)).toList() ??
-            ['0.0', '0.0', '0.0'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Reconhecimento de atividade'),
+          title: Text('TensorFlow Lite Example'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Acelerômetro: $accelerometer'),
-              const SizedBox(height: 20),
-              Text('Predição: $_prediction'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _isRecognizing ? null : _startRecognition,
-                child: const Text('Iniciar Reconhecimento'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: _isRecognizing ? _stopRecognition : null,
-                child: const Text('Parar Reconhecimento'),
-              ),
-              const SizedBox(height: 30),
               Text(
-                _erro,
-                style: const TextStyle(color: Colors.red),
+                'Accelerometer Values:',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'X: ${_accelerometerValues[0].toStringAsFixed(2)}, Y: ${_accelerometerValues[1].toStringAsFixed(2)}, Z: ${_accelerometerValues[2].toStringAsFixed(2)}',
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _isStartButtonDisabled
+                    ? null
+                    : () {
+                        startListeningToAccelerometer();
+                      },
+                child: Text('Start Recognition'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  stopListeningToAccelerometer();
+                  setState(() {
+                    _actions = [];
+                  });
+                },
+                child: Text('Stop Recognition'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Predicted Action:',
+              ),
+              SizedBox(height: 10),
+              Text(
+                _actions.isNotEmpty ? _actions[0] : 'N/A',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ],
           ),

cb7e3fb271057aa7848e6602b020394abc9759a0
lauro <lauro.alexandre@gmail.com>
2024-05-19 10:21:23 -0300

    - Modelo funcionando!!!

--- lib/pages/recognitionpage.dart
+++ lib/pages/recognitionpage.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
@@ -13,12 +14,44 @@ class RecognitionPage extends StatefulWidget {
 }
 
 class _RecognitionPageState extends State<RecognitionPage> {
-  List<String> _actions = [];
+  String _predictedAction = '';
   bool _isModelLoaded = false;
   bool _isListening = false;
   bool _isStartButtonDisabled = false;
   Interpreter? _interpreter;
   List<double> _accelerometerValues = [0.0, 0.0, 0.0];
+
+  final List<String> classes = [
+    'AndandoLeve',
+    'AndandoModerado',
+    'AndandoVigoroso',
+    'DeitadoLeve',
+    'DeitadoModerado',
+    'DeitadoVigoroso',
+    'SentadoLeve',
+    'SentadoModerado',
+    'SentadoVigoroso'
+  ];
+
+  Uint8List _convertToBuffer(List<double> input) {
+    var byteData = ByteData(12);
+    for (int i = 0; i < input.length; i++) {
+      byteData.setFloat32(i * 4, input[i], Endian.little);
+    }
+    return byteData.buffer.asUint8List();
+  }
+
+  int _argmax(List<double> list) {
+    var maxIndex = 0;
+    var maxValue = list[0];
+    for (var i = 1; i < list.length; i++) {
+      if (list[i] > maxValue) {
+        maxValue = list[i];
+        maxIndex = i;
+      }
+    }
+    return maxIndex;
+  }
 
   @override
   void initState() {
@@ -35,6 +68,7 @@ class _RecognitionPageState extends State<RecognitionPage> {
   Future<void> loadModel() async {
     try {
       _interpreter = await Interpreter.fromAsset('assets/rah_model.tflite');
+
       print('Model loaded successfully');
     } catch (e) {
       print('Failed to load model: $e');
@@ -42,17 +76,21 @@ class _RecognitionPageState extends State<RecognitionPage> {
   }
 
   // Predict action
-  Future<void> predictAction(List<List<double>> input) async {
+  Future<void> predictAction(List<double> dataPoint) async {
     if (!_isModelLoaded || _interpreter == null) return;
 
     try {
-      List<Object?> inputList = [input];
-      List<Object?> outputList = [List.filled(1, 0.0, growable: true)];
+      var input = dataPoint.map((e) => e.toDouble()).toList();
+      var inputBuffer = _convertToBuffer(input);
 
-      _interpreter!.run(inputList, outputList);
+      var outputBuffer = Uint8List(4 * classes.length).buffer;
+      _interpreter!.run(inputBuffer, outputBuffer);
+
+      var output = outputBuffer.asFloat32List();
+      var predictedClassIndex = _argmax(output);
 
       setState(() {
-        _actions = outputList[0] as List<String>;
+        _predictedAction = classes[predictedClassIndex];
       });
     } catch (e) {
       print('Failed to run inference: $e');
@@ -63,11 +101,11 @@ class _RecognitionPageState extends State<RecognitionPage> {
   void listenToAccelerometer() {
     accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
-        _accelerometerValues = [event.x ?? 0.0, event.y ?? 0.0, event.z ?? 0.0];
+        _accelerometerValues = [event.x, event.y, event.z];
       });
       if (_isListening) {
         List<double> accelerometerData = _accelerometerValues;
-        predictAction([accelerometerData]);
+        predictAction(accelerometerData);
       }
     });
   }
@@ -86,6 +124,11 @@ class _RecognitionPageState extends State<RecognitionPage> {
     });
   }
 
+  void testModelWithExampleInput() {
+    List<double> exampleDataPoint = [0.1, 0.2, 0.3]; // Example input
+    predictAction(exampleDataPoint);
+  }
+
   @override
   void dispose() {
     if (_interpreter != null) {
@@ -99,46 +142,51 @@ class _RecognitionPageState extends State<RecognitionPage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('TensorFlow Lite Example'),
+          title: const Text('TensorFlow Lite Example'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
+              const Text(
                 'Accelerometer Values:',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               Text(
                 'X: ${_accelerometerValues[0].toStringAsFixed(2)}, Y: ${_accelerometerValues[1].toStringAsFixed(2)}, Z: ${_accelerometerValues[2].toStringAsFixed(2)}',
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _isStartButtonDisabled
                     ? null
                     : () {
                         startListeningToAccelerometer();
                       },
-                child: Text('Start Recognition'),
+                child: const Text('Start Recognition'),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   stopListeningToAccelerometer();
                   setState(() {
-                    _actions = [];
+                    _predictedAction = '';
                   });
                 },
-                child: Text('Stop Recognition'),
+                child: const Text('Stop Recognition'),
               ),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: testModelWithExampleInput,
+                child: const Text('Test Model with Example Input'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
                 'Predicted Action:',
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
-                _actions.isNotEmpty ? _actions[0] : 'N/A',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                _predictedAction.isEmpty ? 'N/A' : _predictedAction,
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ],
           ),
